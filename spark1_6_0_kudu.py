b5af20a4628d9a4f3289fb54d3bad7d8d9b31d93
Ankit Sarraf <asarraf@users.noreply.github.com>
2017-08-25 13:35:33 -0700

    Created park1_6_0_kudu.py

---
+++ spark1_6_0_kudu.py
@@ -0,0 +1,42 @@
+# Dated           : Aug 25, 2017
+# About           : Sample Pyspark (1.6.0) code to count number of Rows in a Kudu Table
+# Pyspark Version : 1.6.0
+# Kudu Version    : 1.2.0
+# Coder           : Ankit Sarraf
+
+import ConfigParser
+from pyspark import SparkContext
+from pyspark import SparkConf
+from pyspark.sql import SQLContext
+
+# Initialize the configuration
+conf = (SparkConf().setMaster("yarn-client").setAppName("KuduSpark_AS"))
+
+# Initialize the SparkContext using the above conf
+sc = SparkContext(conf = conf)
+
+# Initialize the SQLContext using the above SparkContext
+sqlContext = SQLContext(sc)
+
+# Use the ConfigParser
+configParser = ConfigParser.ConfigParser()
+# Load the file that contains the Configuration Parameters
+configParser.read('test.conf')
+
+# Determine the kudu_master as provided in the Config File
+kudu_master = configParser.get('Kudu', 'KuduMaster')
+# Provide the table to be read
+kudu_table = configParser.get('Kudu', 'KuduTable')
+
+# Display Data Read from Config File
+print('KUDU MASTER: ' + kudu_master)
+print('KUDU TABLES: ' + kudu_table)
+
+# Load the table in the memory
+kudu_events_df = sqlContext.read.format('org.apache.kudu.spark.kudu'). \
+        option('kudu.master', kudu_master). \
+        option('kudu.table', kudu_table). \
+        load()
+
+# Display the count of the rows in the table
+print('My Count: ' + str(kudu_events_df.count()))
