3ff9e2077a82350183eded4c0f6bcd3355a2f766
Ankit Sarraf <asarraf@users.noreply.github.com>
2017-08-25 16:09:35 -0700

    Create spark2_1_0_kudu.py

---
+++ spark2_1_0_kudu.py
@@ -0,0 +1,7 @@
+# Dated .         : Aug 25, 2017
+# About           : Sample Pyspark (2.1.0) code to count number of Rows in a Kudu Table
+# Pyspark Version : 2.1.0
+# Kudu Version    : 1.2.0
+# Coder           : Ankit Sarraf
+
+#In-progress
